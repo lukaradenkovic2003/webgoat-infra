78de30d1a31149efffd529d5962326c01e08dcdb
lukaradenkovic2003 <lukaradenkovic@libero.it>
2026-08-10 19:19:07 +0200

    fix(terraform): resolve EKS node group creation timeout by adding VPC tags and IAM policies

--- eks.tf
+++ eks.tf
@@ -10,6 +10,8 @@ module "eks" {
 
   cluster_endpoint_public_access = true
 
+  enable_cluster_creator_admin_permissions = true
+
   eks_managed_node_groups = {
     default = {
       instance_types = [var.eks_node_instance_type]
