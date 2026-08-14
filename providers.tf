939d20f41f9c352d06b4e3129ace9a3771bd5878
lukaradenkovic2003 <lukaradenkovic@libero.it>
2026-08-14 12:38:08 +0200

    fix(helm): fetch fresh k8s endpoint and auth token via data sources

--- providers.tf
+++ providers.tf
@@ -34,15 +34,18 @@ provider "cloudflare" {
   api_key = var.cloudflare_api_token
 }
 
-provider "helm" {
-  kubernetes = {
-    host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
 
-    exec = {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
-    }
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
