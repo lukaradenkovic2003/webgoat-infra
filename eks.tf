e82b286300ac1385518d155fe56bf2344844c525
lukaradenkovic2003 <lukaradenkovic@libero.it>
2026-08-14 13:29:59 +0200

    refactor(ci): use dynamic cluster name from terraform output for helm

--- eks.tf
+++ eks.tf
@@ -5,6 +5,8 @@ module "eks" {
   cluster_name    = "${var.project_name}-cluster"
   cluster_version = var.eks_cluster_version
 
+  enable_cluster_creator_admin_permissions = true
+
   cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
